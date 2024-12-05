bb09f323ca643e8de9774fc31f5c8a3581bcbe28
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-05 12:11:22 -0500

    feat: Add field focus state
    - Modify field styles for focus state
    - Add event listener for input on focus and blur to update field class

---
+++ assets/js/field.js
@@ -0,0 +1,31 @@
+(function () {
+	// Enable strict mode.
+	"use strict";
+
+	// Define global object if it doesn't exist.
+	if ("object" !== typeof window.LQ) {
+		window.LQ = {};
+	}
+
+	LQ.field = () => {
+		const inputs = document.querySelectorAll( '.lq-field input' );
+
+		inputs.forEach( ( input, index ) => {
+			input.addEventListener( 'focus', ( event ) => {
+				const wrapper = event.currentTarget.parentElement;
+				const field = wrapper.parentElement;
+
+				field.classList.add( 'lq-field--focus' );
+			}, true );
+
+			input.addEventListener( 'blur', ( event ) => {
+				const wrapper = event.currentTarget.parentElement;
+				const field = wrapper.parentElement;
+
+				field.classList.remove( 'lq-field--focus' );
+			}, true );
+		});
+	};
+
+	LQ.field();
+})();
